d0f520455f2cc6cf103f3a01778b8445008e5c70
Vikrant Borawake <vikrant.borawake@questionpro.com>
2025-02-25 17:57:45 +0530

    Added private variables

--- classes/Customer.ts
+++ classes/Customer.ts
@@ -1,30 +1,20 @@
 import { DiscountTypeEnum } from "../enums/discountType";
+import { getDiscountValueFromDiscountType } from "../util/discount.util";
 
 export class Customer {
-  customerName: string;
-  discountType: DiscountTypeEnum; // "Regular", "Premium", "VIP"
-  discount: number;
+  private readonly customerName: string;
+  private readonly discountType: DiscountTypeEnum; // "Regular", "Premium", "VIP"
 
   constructor(customerName: string, discountType: DiscountTypeEnum) {
     this.customerName = customerName;
     this.discountType = discountType;
-    this.setDiscount();
   }
 
-  setDiscount(): void {
-    this.discount = this.getDiscountValue(this.discountType);
+  getCustomerName(): string {
+    return this.customerName;
   }
 
-  getDiscountValue(discountType: DiscountTypeEnum): number {
-    switch (discountType) {
-      case DiscountTypeEnum.REGULAR:
-        return 0.05;
-      case DiscountTypeEnum.PREMIUM:
-        return 0.1;
-      case DiscountTypeEnum.VIP:
-        return 0.2;
-      default:
-        return 0;
-    }
+  getCustomerDiscountValue(): number {
+    return getDiscountValueFromDiscountType(this.discountType);
   }
 }
